564e183e2f29a548ad4ea4620d7082b8af5327bf
maryam_daneshjoo <59700522+marya-d@users.noreply.github.com>
2020-05-17 09:24:44 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'ClassOfScreens.dart';
 import 'ShopBottomNavigator-1.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 //mypacks
-import 'package:mychangehome/components/horizontal_listview.dart';
 import 'package:mychangehome/components/products.dart';
 void main() => (runApp(First()));
 class First extends StatelessWidget{
@@ -26,8 +25,6 @@ class First extends StatelessWidget{
         //const Locale('en'), // English
         const Locale('fa'), // Farsi
       ],
-
-
       title: 'first',
       debugShowCheckedModeBanner: false
       ,
@@ -56,10 +53,13 @@ class FirstScreenState extends StatefulWidget{
 class FirstScreen extends State<FirstScreenState>{
   @override
   Widget build(BuildContext context) {
+
+    var labels =['مبدل','ایسیوکیت','اسیلوسکوپ','پراگرامر','لوازم جانبی'];
+
     Widget image_carousel= new Container(
       height: 200.0,
       child: new Carousel(
-        boxFit: BoxFit.cover,
+        boxFit: BoxFit.fill,
         images: [
           AssetImage('images/carosalImages/classbanner.jpg'),
           AssetImage('images/carosalImages/classbanner2.jpg'),
@@ -72,18 +72,19 @@ class FirstScreen extends State<FirstScreenState>{
         indicatorBgPadding: 12.0,
       ),
     );
-    var labels =['مبدل','ایسیوکیت','اسیلوسکوپ','پراگرامر','لوازم جانبی'];
 
-    return Scaffold(/*appBar: AppBar(centerTitle: true,title: Text(' فروشگاه',style: TextStyle(color: Colors.blueGrey,fontFamily: "Bamshad"),), backgroundColor: Colors.black12, elevation:0,actions: <Widget>[
-      IconButton(
-        icon: Icon(Icons.shop,color: Colors.black45,),
-      ),
-      IconButton(
-        icon: Icon(Icons.search,color: Colors.blueGrey[600],),
-        onPressed: ()=>(Navigator.pushNamed(context, '/eleven')
-        ),),
+    return Scaffold(
+      appBar:AppBar(centerTitle: true,title: Text(' فروشگاه',style: TextStyle(color: Colors.blueGrey,fontFamily: "Bamshad"),), backgroundColor: Colors.pink[500], elevation:0,actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.shop,color: Colors.black45,),
+        ),
+        IconButton(
+          icon: Icon(Icons.search,color: Colors.blueGrey[600],),
+          onPressed: ()=>(Navigator.pushNamed(context, '/eleven')
+          ),),
 
-    ], ),*/
+      ], ),
+
         bottomNavigationBar: ShopBottomNavigator(),
         floatingActionButton: FloatingActionButton(backgroundColor: Colors.deepPurple[500],child: Icon(Icons.add),
           onPressed: ()=>(Navigator.pushNamed(context, '/third')
@@ -97,78 +98,86 @@ class FirstScreen extends State<FirstScreenState>{
         ),
           ListTile(leading: Icon(Icons.home), title: Text("Home")),
           ListTile(leading: Icon(Icons.grid_on), title: Text("Products")),
-          ListTile(leading: Icon(Icons.contacts), title: Text("Contact Us")),
+          ListTile(leading:  Icon(Icons.contacts), title: Text("Contact Us")),
 
         ],),),
 
-        body: new ListView(
+        body: Container(
+          color: Colors.black12,
+          child: ListView(
 
-          children:<Widget>[
-            //customAppBar1(),
-            customAppBar(),
-            Container(height: 5.0,),
-            image_carousel,
+            children:<Widget>[
+              //customAppBar1(),
+              customAppBar1(),
+              Container(height: 5.0,),
+              Container(
+                padding: EdgeInsets.fromLTRB(24, 36, 24, 24),
+                child: image_carousel,
 
-            new Padding(padding: const EdgeInsets.all(18.0),
-              child: new Text('دسته بندی', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18.0)),
-            ),
-            Container(
-              height: 120.0,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount:  labels.length,
-                itemBuilder: (BuildContext context, int index){
-                  return Container(
-                    margin: EdgeInsets.all(10.0),
-                    width: 160.0,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.0),
-                        gradient: LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: [
-//                              Color(0xFFD45253),
-//                              Color(0xFF9E1F28),
-                            Colors.pinkAccent[200],
-                              Colors.pinkAccent[400],
-                            ]
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Color(0xFF9E1F28),
-                            offset: Offset(0.0,0.2),
-                            blurRadius: 6.0,
-                          )
-                        ]
-                    ),
-                    child: Center(child: Text(labels[index],style: TextStyle(fontSize: 16.0),),),
-                  );
-                },
               ),
 
-            ),
+
+              new Padding(padding: const EdgeInsets.all(18.0),
+                child: new Text('دسته بندی', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18.0)),
+              ),
+              Container(
+                height: 120.0,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount:  labels.length,
+                  itemBuilder: (BuildContext context, int index){
+                    return Container(
+                      margin: EdgeInsets.all(10.0),
+                      width: 160.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          gradient: LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+//                              Color(0xFFD45253),
+//                              Color(0xFF9E1F28),
+                                Colors.pinkAccent[200],
+                                Colors.pinkAccent[400],
+                              ]
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color(0xFF9E1F28),
+                              offset: Offset(0.0,0.2),
+                              blurRadius: 6.0,
+                            )
+                          ]
+                      ),
+                      child: Center(child: Text(labels[index],style: TextStyle(fontSize: 16.0),),),
+                    );
+                  },
+                ),
+
+              ),
 
 
 
 
-           /* //padding widget
-            new Padding(padding: const EdgeInsets.all(18.0),
-            child: new Text('دسته بندی', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18.0)),
-            ),
-            // listscroll
+             /* //padding widget
+              new Padding(padding: const EdgeInsets.all(18.0),
+              child: new Text('دسته بندی', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18.0)),
+              ),
+              // listscroll
 
-            HorizontalList(),*/
+              HorizontalList(),*/
 
-            new Padding(padding: const EdgeInsets.all(18.0),
-              child: new Text('محصولات', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18.0)),
-            ),
-            Container(
-              height: 320.0,
-              child: Products(),
-            ),
+              new Padding(padding: const EdgeInsets.all(18.0),
+                child: new Text('محصولات', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18.0)),
+              ),
+              Container(
+                height: 320.0,
+                child: Products(),
+              ),
 
 
-          ]
+            ]
+          ),
         )
     );
   }
@@ -185,14 +194,18 @@ class customAppBar1 extends StatelessWidget{
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          Row(
+          /*Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Icon(Icons.menu,size: 32,color: Colors.white,),
+              IconButton(
+                icon: Icon(Icons.menu,size: 32,color: Colors.white,),
+                onPressed: ()=>(Navigator.pushNamed(context, '/seven')
+                ),),
               Text('طراحان نوین مدار',style: TextStyle(fontSize: 18.0),),
               Icon(Icons.shopping_cart,size: 32,color: Colors.white,),
             ],
-          ),
+
+          ),*/
           //SizedBox(height: 28,),
           searchBox(),
 
@@ -210,14 +223,15 @@ Widget customAppBar(){
     child: Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
-        Row(
+       /* Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
+
             Icon(Icons.menu,size: 32,color: Colors.white,),
             Text('طراحان نوین مدار',style: TextStyle(fontSize: 18.0),),
             Icon(Icons.shopping_cart,size: 32,color: Colors.white,),
           ],
-        ),
+        ),*/
         SizedBox(height: 28,),
         searchBox(),
 
